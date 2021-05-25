e631bb992696b914023e508dcea8a3067f3f8179
Usamaalikhan654 <usamaalikhan654@gmail.com>
2021-05-25 11:49:02 +0500

    1.4.8
    version update

--- hepl-master/src/index.js
+++ hepl-master/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+  <HashRouter>
     <App />
+   </HashRouter> 
   </React.StrictMode>,
   document.getElementById("root")
 );
