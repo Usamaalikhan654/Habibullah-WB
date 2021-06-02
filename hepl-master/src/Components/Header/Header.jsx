bfc13bdcf218fcd9be4aac0d8025f89dce3c9c18
Usamaalikhan654 <usamaalikhan654@gmail.com>
2021-06-02 12:01:20 +0500

    3.2.0
    version initially pushed

--- hepl-master/src/Components/Header/Header.jsx
+++ hepl-master/src/Components/Header/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
       <div className={styles.menuContainer}>
         <div className={styles.menuBox}>
           <div className={styles.menuContainerBox}>
-            <a href="/"><p className={styles.menuItem}>HOME</p></a>
+            <Link to="/"><p className={styles.menuItem}>HOME</p></Link>
             <a href="/about"><p className={styles.menuItem}>ABOUT US</p></a>
             <a href="/Product"><p className={styles.menuItem}>PRODUCT LINE</p></a>
             <a href="/Machine"><p className={styles.menuItem}>MACHINERY AND TOOLS</p></a>
