33abf73d9bcceb07aee75b6f5149af0dffc53e30
Usamaalikhan654 <usamaalikhan654@gmail.com>
2021-06-02 11:38:10 +0500

    3.1.8
    version

--- hepl-master/src/App.js
+++ hepl-master/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch , Redirect } from "react-router-dom";
+import {  BrowserRouter as Router, Route, Switch , Redirect } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import "./config/styles.scss";
 import About from "./Pages/About Us/About";
@@ -9,7 +9,7 @@ import Contact from "./Pages/Contact Us/Contact";
 
 export default function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/About" component={About}/>
@@ -17,6 +17,6 @@ export default function App() {
         <Route exact path="/Machine" component={Machine}/>
         <Route exact path="/Contact" component={Contact}/>  
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
