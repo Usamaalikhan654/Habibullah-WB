b4805a16430c9aa386009ec59cf0020a9761ddb4
Usamaalikhan654 <usamaalikhan654@gmail.com>
2021-06-07 16:07:02 +0500

    3.4.8
    version is ready inshallah

--- hepl-master/src/Pages/Contact Us/Form.jsx
+++ hepl-master/src/Pages/Contact Us/Form.jsx
@@ -20,11 +20,11 @@ import emailjs from 'emailjs-com';
 
 
   export default function Form() {
-
+   
     function sendEmail(e) {
       e.preventDefault();
   
-      emailjs.sendForm('gmail', 'template_dyt8nbj', e.target, 'user_bgqJpE58sM4p2OVzSDoE6')
+      emailjs.sendForm('service_qr4ch2m', 'template_dyt8nbj', e.target, 'user_bgqJpE58sM4p2OVzSDoE6')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
@@ -32,7 +32,6 @@ import emailjs from 'emailjs-com';
         });
     }
 
-    const reload=()=>window.location.reload();
 
          
 
@@ -146,29 +145,29 @@ import emailjs from 'emailjs-com';
         <div className="row-6">
           <div className="column-7">
           <span className="form-pic14"><img src={formpic14} style={{height: "40px"}}/></span>
-          <input type="text" className="form-control"  placeholder="Enter Your Name" required/>
+          <input type="text" className="form-control" name="name"  placeholder="Enter Your Name" required/>
           </div>
 
           <div className="column-7">
           <span className="form-pic14"><img src={formpic10} style={{height: "28px"}}/></span>
-          <input type="text" className="form-control"   placeholder="Enter Your Number" required/>
+          <input type="text" className="form-control" name="number"  placeholder="Enter Your Number" required/>
           </div>
 
           <div className="column-7">
           <span className="form-pic11"><img src={formpic11} style={{height: "62px"}}/></span>
-          <input type="text" className="form-control"  placeholder="Enter Your Email" required/>
+          <input type="text" className="form-control" name="email"  placeholder="Enter Your Email" required/>
           </div>
         </div>
 
         <div className="row-6">
           <div className="column-7">
           <span className="form-pic12"><img src={formpic12} style={{height: "50px"}}/></span>
-          <input type="text" className="form-control"  placeholder="Enter Your Subject" required/>
+          <input type="text" className="form-control" name="subject" placeholder="Enter Your Subject" required/>
           </div>
 
           <div className="column-7">
           <span className="form-pic15"><img src={formpic15} style={{height: "50px"}}/></span>
-          <input type="text" className="form-control"  placeholder="Enter Your Company Name " required/>
+          <input type="text" className="form-control" name="company" placeholder="Enter Your Company Name " required/>
           </div>
 
         </div>
@@ -176,13 +175,13 @@ import emailjs from 'emailjs-com';
            <div className="row-6">
                 <div className="column-8">
                 <span className="form-pic13"><img src={formpic13} style={{height: "50px"}}/></span>
-        <textarea className="text"  placeholder="Enter Your Message Here"></textarea>
+        <textarea className="text" name="message" placeholder="Enter Your Message Here"></textarea>
                 </div>
            </div>
            <div className="row-6">
                        <div className="column-9">
                        
-                      <button onClick={reload} className="form-col">VERIFY AND SEND MESSAGE</button>
+                      <button className="form-col">VERIFY AND SEND MESSAGE</button>
                        </div>
                  </div>       
         </form>  
