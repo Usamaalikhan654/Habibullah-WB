b126b375167971077106da922a7e6aee83e275c9
Usamaalikhan654 <usamaalikhan654@gmail.com>
2021-06-11 12:53:15 +0500

    version 3.8.0
    working on form

--- hepl-master/src/Pages/Contact Us/Form.jsx
+++ hepl-master/src/Pages/Contact Us/Form.jsx
@@ -34,7 +34,7 @@ import Axios from 'axios';
     //     e.target.reset()
     // }
 
-    const url="1cc49d8569ee846fcacfedfaa88c2b78"
+    const url="https://jsonplaceholder.typicode.com/users"
     const [data,setData] = useState ({
       user_name:"",
       phone_number:"",
